875c2b4463ddf25b68af9ee9e94dffb935a37ee3
Mary Joy A. Bernales <ms.maryjoybernales@gmail.com>
2025-08-15 18:35:20 +0800

    Dart User Input Validation

--- Act4.dart
+++ Act4.dart
@@ -1,22 +1,12 @@
-import 'dart:io';
-
 void main() {
- 
-  stdout.write("Enter your name: ");
-  String name = stdin.readLineSync() ?? '';
   
-  stdout.write("Enter your email: ");
-  String email = stdin.readLineSync() ?? '';
-  
-  stdout.write("Enter your subject: ");
-  String subject = stdin.readLineSync() ?? '';
-  stdout.write("Enter your course: ");
-  String course = stdin.readLineSync() ?? '';
+  String name = "Mary Joy Bernales";
+  String email = "ms.maryjoybernales@gmail.com";
+  String subject = "Basic Mobile Computing";
+  String course = "Bachelor of Science in Information Technology ";
 
-  
-  
-  print("Name : $name");
-  print("Email : $email");
+  print("Name: $name");
+  print("Email: $email");
   print("Subject: $subject");
-  print("Course : $course");
+  print("Course: $course");
 }
